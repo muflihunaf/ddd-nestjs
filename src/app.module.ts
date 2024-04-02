b3589a69df84d291d98f59383666bb68aaa6fcee
muflihunaf <firmanedam19@gmail.com>
2024-04-02 21:11:22 +0700

    [Config] Configure database

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+// src/app.module.ts
+
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import databaseConfig from './config/database.config';
+// import { BookModule } from './books/book.module';
+// import { UserModule } from './users/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri'),
+      }),
+      inject: [ConfigService],
+    }),
+    // BookModule,
+    // UserModule,
+  ],
+})
+export class AppModule {}
